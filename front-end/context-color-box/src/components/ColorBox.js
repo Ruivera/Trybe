f9547a3e09956a4a575203937c9f91d7f49f555c
Bernardo Luz <beluz.mario@gmail.com>
2022-10-24 14:28:42 -0300

    tudo feito

--- front-end/context-color-box/src/components/ColorBox.js
+++ front-end/context-color-box/src/components/ColorBox.js
@@ -3,33 +3,11 @@ import React from 'react';
 import '../styles/box.css';
 import MyContext from './MyContext';
 
-/* class ColorBox extends React.Component {
-  render() {
-    return(
-        <MyContext.Consumer>
-            {(value) => {
-            const { backgroundArray, indexColor, changeColor } = value;
-            
-            return (
-            <button
-                type="button"
-                className="box"
-                style={ { backgroundColor: backgroundArray[indexColor] } }
-                onClick={ changeColor }
-            >
-                Click me to change my color!
-            </button>
-            )
-            }}
-        </MyContext.Consumer>
-    )
-  }
-} */
-
 class ColorBox extends React.Component {
     render() {
         const value = this.context;
         const { backgroundArray, indexColor, changeColor } = value;
+        console.log(value);
 
     return(
             <button
